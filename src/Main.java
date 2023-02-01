a71fd3b9082ad63f9a4e9fa4a7a576ec2f61de67
91112021 <dalebolido@gmail.com>
2023-02-01 12:36:58 -0800

    agagaga

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
-public class Main {
+import java.util.ArrayList;
+
+public class Main
+{
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
-    }
-}
+
+        ArrayList<Integer> funArray = new ArrayList<Integer>();
+
+        for(int row = 0; row < 5; row++)
+        {
+            for(int column = 0; column < 5; column++)
+            {
+                funArray.add();
+            }//end inner for loop
+
+        }//end out for loop
+
+    }//end main method
+}//end class
