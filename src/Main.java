572603a9f488cca029a9ee0032748532ee620d2e
91112021 <dalebolido@gmail.com>
2023-02-23 12:37:43 -0800

    in the middle of problem 10

--- src/Main.java
+++ src/Main.java
@@ -11,14 +11,9 @@ public class Main
         for (int row = 0; row < fun.length; row++)
             for(int col = 0; col < fun[0].length; col++)
                 //fun[row][col] = col; // problem 1
-                //fun[row][col] = row; //problem 2
+                fun[row][col] = row; //problem 2
 
-        for (int r = 0; r < fun.length; r++)
-        {
-            for (int c = 0; c < fun[0].length; c++)
-                System.out.print(fun[r][c] + " ");
-            System.out.println();
-        }
+        printString(fun);
 
 
         System.out.println("Problem 3\n");
@@ -26,26 +21,64 @@ public class Main
             for(int col = 0; col < fun[0].length; col++)
                 fun[row][col] = ((row + 1) * 5) - col;
 
-        for (int r = 0; r < fun.length; r++)
-        {
-            for (int c = 0; c < fun[0].length; c++)
-                System.out.print(fun[r][c] + " ");
-            System.out.println();
-        }
+        printString(fun);
+
 
         System.out.println("Problem 9\n");
-        for (int row = 0; row < fun.length; row++)
-            for(int col = 0; col < fun[0].length; col++)
-                fun[row][col] = col +1;
 
+        int counter = 1;
+        for (int row = 0; row < fun.length; row++)
+        {
+            for (int col = row; col < fun[0].length; col++)
+            {
+                fun[row][col] = counter;
+                counter++;
+            }
+        }
+
+        for (int row = 1; row < fun.length; row ++)
+        {
+            for(int col = 0; col < row; col++)
+            {
+                fun[row][col] = counter;
+                counter++;
+            }
+        }
+        printString(fun);
+        //resetArray(fun);
+
+///////////////////end problem 9\\\\\\\\\\\\\\\\\\\\
+
+        counter = 1;
+        System.out.println("Problem 10\n");
+
+        for (int row = 0; row < fun.length; row++)
+        {
+            for (int col = row; col < fun[0].length; col++)
+            {
+                fun[row][col] = counter;
+                counter++;
+            }
+            counter += row;
+        }
+        printString(fun);
+
+    }//end main method
+
+    public static void printString(int[][] fun)
+    {
         for (int r = 0; r < fun.length; r++)
         {
             for (int c = 0; c < fun[0].length; c++)
                 System.out.print(fun[r][c] + " ");
             System.out.println();
         }
+    }//end printString
+    public static void resetArray(int[][] fixed)
+    {
+        for (int r = 0; r < fixed.length; r++)
+            for (int c = 0; c < fixed[0].length; c++)
+                fixed[r][c] = 0;
+    }
 
-
-
-    }//end main method
 }//end class
